b0826b788e554ce35867a9f4d1b00a7c21dfa0d8
Alex Mendes <alex.mendes@bl.uk>
2017-06-20 14:57:42 +0100

    Move to webpack build

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: "./src/main.js"
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
